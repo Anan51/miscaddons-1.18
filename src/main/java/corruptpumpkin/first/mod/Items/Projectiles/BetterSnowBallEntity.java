169852000c64de75ca4aa68b3e11b4379417ac4e
Magik <borolesaahil@gmail.com>
2022-03-06 19:36:27 -0800

    Added a custom type of snowball to the game.

---
+++ src/main/java/corruptpumpkin/first/mod/Items/Projectiles/BetterSnowBallEntity.java
@@ -0,0 +1,64 @@
+package corruptpumpkin.first.mod.Items.Projectiles;
+
+import corruptpumpkin.first.mod.FirstMod;
+import corruptpumpkin.first.mod.Packets.EntitySpawnPacket;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
+import net.minecraft.item.Item;
+import net.minecraft.network.Packet;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.world.World;
+
+public class BetterSnowBallEntity extends ThrownItemEntity {
+    public BetterSnowBallEntity(EntityType<? extends ThrownItemEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    public BetterSnowBallEntity(EntityType<? extends ThrownItemEntity> entityType, double d, double e, double f, World world) {
+        super(entityType, d, e, f, world);
+    }
+
+    public BetterSnowBallEntity(EntityType<? extends ThrownItemEntity> entityType, LivingEntity livingEntity, World world) {
+        super(entityType, livingEntity, world);
+    }
+
+    @Override
+    protected Item getDefaultItem() {
+        return null;
+
+    }
+    protected void onEntityHit(EntityHitResult entityHitResult) { // called on entity hit.
+        super.onEntityHit(entityHitResult);
+        Entity entity = entityHitResult.getEntity(); // sets a new Entity instance as the EntityHitResult (victim)
+        entity.damage(DamageSource.thrownProjectile(this, this.getOwner()), 1); // deals damage
+
+        if (entity instanceof LivingEntity livingEntity) { // checks if entity is an instance of LivingEntity (meaning it is not a boat or minecart)
+
+            livingEntity.addStatusEffect((new StatusEffectInstance(StatusEffects.SLOWNESS, 60, 3))); // applies a status effect
+            livingEntity.setInPowderSnow(true);
+            livingEntity.playSound(SoundEvents.ENTITY_LIGHTNING_BOLT_IMPACT, 2F, 5F); // plays a sound for the entity hit only
+        }
+    }
+
+    protected void onCollision(HitResult hitResult) { // called on collision with a block
+        super.onCollision(hitResult);
+        if (!this.world.isClient) { // checks if the world is client
+            this.world.sendEntityStatus(this, (byte)3); // particle?
+            this.kill(); // kills the projectile
+        }
+
+    }
+    @Override
+    public Packet<?> createSpawnPacket() {
+        return EntitySpawnPacket.create(this, FirstMod.PacketID);
+    }
+
+}
