4333751a8a5ed9104733bef0ae862dcb9a3563a6
Magik <borolesaahil@gmail.com>
2022-03-08 18:08:05 -0800

    Added item requirment to fire cannons

--- src/main/java/corruptpumpkin/first/mod/Functionalities/EnergyBallCannonItem.java
+++ src/main/java/corruptpumpkin/first/mod/Functionalities/EnergyBallCannonItem.java
@@ -1,7 +1,10 @@
 package corruptpumpkin.first.mod.Functionalities;
 
+import corruptpumpkin.first.mod.FirstMod;
 import corruptpumpkin.first.mod.Items.Projectiles.EnergyBoltEntity;
+import corruptpumpkin.first.mod.Items.Projectiles.EnergyBoltItem;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.EggItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
@@ -16,17 +19,24 @@ public class EnergyBallCannonItem extends Item {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
-        if (playerEntity.getItemUseTime() < 60) {
-            if (!world.isClient()) {
-                EnergyBoltEntity snowball = new EnergyBoltEntity(world, playerEntity);
-                snowball.setPos(playerEntity.getX(), playerEntity.getY() + 1.62, playerEntity.getZ());
-                snowball.setVelocity(playerEntity, playerEntity.getPitch(), playerEntity.getHeadYaw(), 0.0f, 3.0f, 0.0f);
+        ItemStack itemStack = new ItemStack(FirstMod.ENERGY_BOLT_ITEM);
+        if (playerEntity.getInventory().contains(itemStack)) {
+            if (playerEntity.getItemUseTime() < 60) {
+                if (!world.isClient()) {
+                    EnergyBoltEntity snowball = new EnergyBoltEntity(world, playerEntity);
+                    snowball.setPos(playerEntity.getX(), playerEntity.getY() + 1.62, playerEntity.getZ());
+                    snowball.setVelocity(playerEntity, playerEntity.getPitch(), playerEntity.getHeadYaw(), 0.0f, 3.0f, 0.0f);
 
-                world.spawnEntity(snowball);
+                    world.spawnEntity(snowball);
+                }
+            } else {
+                playerEntity.stopUsingItem();
             }
-        } else {
-            playerEntity.stopUsingItem();
+            if (!playerEntity.getAbilities().creativeMode) {
+                itemStack.decrement(1);
+            }
         }
+
 
 
 
--- src/main/java/corruptpumpkin/first/mod/Functionalities/SnowBallCannonItem.java
+++ src/main/java/corruptpumpkin/first/mod/Functionalities/SnowBallCannonItem.java
@@ -1,9 +1,11 @@
 package corruptpumpkin.first.mod.Functionalities;
 
+import corruptpumpkin.first.mod.FirstMod;
 import corruptpumpkin.first.mod.Items.Projectiles.BetterSnowBallEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
@@ -13,19 +15,24 @@ public class SnowBallCannonItem extends Item {
     public SnowBallCannonItem(Settings settings) {
         super(settings);
     }
-
+    ItemStack itemStack = new ItemStack(FirstMod.BETTER_SNOWBALL_ITEM);
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
-        if (playerEntity.getItemUseTime() < 60) {
-            if (!world.isClient()) {
-                BetterSnowBallEntity snowball = new BetterSnowBallEntity(playerEntity, world);
-                snowball.setPos(playerEntity.getX(), playerEntity.getY() + 1.62, playerEntity.getZ());
-                snowball.setVelocity(playerEntity, playerEntity.getPitch(), playerEntity.getHeadYaw(), 0.0f, 3.0f, 0.0f);
+        if (playerEntity.getInventory().contains(itemStack)) {
+            if (playerEntity.getItemUseTime() < 60) {
+                if (!world.isClient()) {
+                    BetterSnowBallEntity snowball = new BetterSnowBallEntity(playerEntity, world);
+                    snowball.setPos(playerEntity.getX(), playerEntity.getY() + 1.62, playerEntity.getZ());
+                    snowball.setVelocity(playerEntity, playerEntity.getPitch(), playerEntity.getHeadYaw(), 0.0f, 3.0f, 0.0f);
 
-                world.spawnEntity(snowball);
+                    world.spawnEntity(snowball);
+                }
+            } else {
+                playerEntity.stopUsingItem();
             }
-        } else {
-            playerEntity.stopUsingItem();
+            if (!playerEntity.getAbilities().creativeMode) {
+                itemStack.decrement(1);
+            }
         }
 
 
